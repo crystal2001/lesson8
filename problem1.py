7e5b62a3d4d995589ee34ed072508dac9373df7e
crystal2001 <43784510+crystal2001@users.noreply.github.com>
2018-12-12 12:37:45 -0500

    1

---
+++ problem1.py
@@ -0,0 +1,21 @@
+print('-' * 70)
+print('Morbid Predictions Program')
+
+print()
+
+print('Description: This program asks yof for your current age and tells you the year that you will die (on average)')
+
+print()
+
+age = input('What is your age today? ')
+age = int(age)
+
+result = 79 - age 
+year = result + 2018
+print('...thinking...')
+print()
+print('...thinking...')
+print()
+print('...thinking...')
+
+print('You will die in the year...' + str(year))
